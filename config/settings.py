daad76f07bc79618bbdaad54622e02d44c228936
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-09 22:32:18 -0300

    Configured HTTPS for development environnment

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3thd party
+    'django_extensions',
 ]
 
 MIDDLEWARE = [

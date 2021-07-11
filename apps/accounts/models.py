446a0edbf2faf2c1b8dfc9c993a26d7a5114c419
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-11 11:36:39 -0300

    Implemented custom user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+"""
+    A implementação do custom user model é altamente recomendada na documentação do
+    Django 3.x.
+    Caso seja necessário customizar o user model no meio do projeto, será mais trabalhoso
+    do que já iniciar com o custom user model antes do primeiro migrate
+"""
+
+
+class CustomUser(AbstractUser):
+    pass

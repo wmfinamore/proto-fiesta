cd1befd078e680eee49b3eebb5c78cec75c0fc2b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-11 16:41:15 -0300

    Configured debug toolbar

--- config/settings.py
+++ config/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
     # 3thd
     'django_extensions',
-    # 'debug_toolbar',
+    'debug_toolbar',
 
     # local
     'accounts',
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # DEBUG TOOLBAR CONFIGURATION
 
-# hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-# INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
--- config/urls.py
+++ config/urls.py
@@ -22,9 +22,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-# if settings.DEBUG:
-#     import debug_toolbar
-#
-#     urlpatterns = [
-#         path('__debug__', include(debug_toolbar.urls))
-#     ] + urlpatterns
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__', include(debug_toolbar.urls))
+    ] + urlpatterns

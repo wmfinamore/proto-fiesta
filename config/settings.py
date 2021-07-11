42eca8c826192c1a0dabecdb68219b1c5ddf3226
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-11 15:57:10 -0300

    Created and registerde app core. Implemented route to home template

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     # local
     'accounts',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
 ]
 

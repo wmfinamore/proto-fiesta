3d6ddedcb6124df78302602a859d81b4a3c6218b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-12 19:55:30 -0300

    Implemented custom forms for customUser model

---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+# get_user_model retorna o modelo indicado em AUTH_USER_MODEL no arquivo settings.py
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username',)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'username', 'cpf', 'matricula')

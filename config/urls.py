40dde64be451dc92ba945f6571fa05cf72c20c26
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-12 22:27:52 -0300

    Included route to authentication views

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 # routes for activate debug toolbar when django debug mode is true

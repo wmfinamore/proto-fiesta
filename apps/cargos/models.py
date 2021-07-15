c546b74fb35e73b1168f30b49232747307287565
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-14 22:58:39 -0300

    Maked field jornada nullable

--- apps/cargos/models.py
+++ apps/cargos/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Cargo(models.Model):
     nome = models.CharField(max_length=50, null=True, blank=True)
     classe = models.CharField(max_length=10, null=True, blank=True)
-    jornada = models.IntegerField()
+    jornada = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.classe + ' - '+self.nome

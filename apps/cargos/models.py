d0df37ce5df9c657da6abd652f102e38a1fb632c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-16 22:16:41 -0300

    Added get_absolute_url in cargo model

--- apps/cargos/models.py
+++ apps/cargos/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Cargo(models.Model):
     nome = models.CharField(max_length=50, null=True, blank=True)
     classe = models.CharField(max_length=10, null=True, blank=True)
     jornada = models.IntegerField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('cargos_lista')
 
     def __str__(self):
         return self.classe + ' - '+self.nome

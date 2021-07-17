e1205afc1770f15dfdd6caba895e2021a76fc16a
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-16 21:46:38 -0300

    Implemented class based view to create new cargo

--- apps/cargos/views.py
+++ apps/cargos/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import (
+                                    ListView,
+                                    CreateView,
+                                 )
 from .models import Cargo
 
 
 class CargosListView(ListView):
     model = Cargo
     context_object_name = 'cargos'
+
+
+class CargoCreateView(CreateView):
+    model = Cargo

f6b62a7f6a5811d735897f1e696f04712d4e5288
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-16 22:26:18 -0300

    Implemented route to edit Cargo

--- apps/cargos/urls.py
+++ apps/cargos/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.CargosListView.as_view(), name='cargos_lista'),
     path('novo/', views.CargoCreateView.as_view(), name='cargo_novo'),
+    path('editar/<int:pk>/', views.CargoEditView.as_view(), name='cargo_editar'),
 ]

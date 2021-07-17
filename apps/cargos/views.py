69c71b2663dc0cbb67b77d5b8193dac9d23c518f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-16 22:31:43 -0300

    Implemented CargoEdit view

--- apps/cargos/views.py
+++ apps/cargos/views.py
@@ -2,6 +2,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import (
                                     ListView,
                                     CreateView,
+                                    UpdateView,
                                  )
 from .models import Cargo
 
@@ -16,3 +17,8 @@ class CargoCreateView(SuccessMessageMixin, CreateView):
     fields = ['classe', 'nome', 'jornada',]
     success_url = '/cargos/'
     success_message = "Cargo foi criado com sucesso!"
+
+
+class CargoEditView(UpdateView):
+    model = Cargo
+    fields = ['classe', 'nome', 'jornada',]

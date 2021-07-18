7e1ef8c9b20e9d23d0d593939489559e2c3d0f1a
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-18 19:51:40 -0300

    Implemented route to VinculoList view

--- apps/cargos/urls.py
+++ apps/cargos/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.CargosListView.as_view(), name='cargos_lista'),
+    path('vinculos/', views.VinculosListView.as_view(), name='vinculos_lista'),
     path('novo/', views.CargoCreateView.as_view(), name='cargo_novo'),
     path('editar/<int:pk>/', views.CargoEditView.as_view(), name='cargo_editar'),
     path('excluir/<int:pk>/', views.CargoDeleteView.as_view(), name='cargo_excluir'),

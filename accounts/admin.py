6c9c9b0a5a6b787a4e0cdb08b71f1a6ff94972db
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-18 00:09:26 -0300

    Refactored customUserAdmin class

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.utils.translation import gettext, gettext_lazy as _
 
 
 CustomUser = get_user_model()
@@ -11,8 +12,15 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'cpf')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
     list_display = ['email', 'username', 'cpf',]
-    list_editable = ['cpf', ]
 
 
 admin.site.register(CustomUser, CustomUserAdmin, )

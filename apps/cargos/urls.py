4ff5a1a788ba07308c3c15667d37ddda217e986c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-19 22:19:45 -0300

    Implemented route to VinculosCreate view

--- apps/cargos/urls.py
+++ apps/cargos/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 
 urlpatterns = [
+    # Cargos URL's
     path('', views.CargosListView.as_view(), name='cargos_lista'),
-    path('vinculos/', views.VinculosListView.as_view(), name='vinculos_lista'),
     path('novo/', views.CargoCreateView.as_view(), name='cargo_novo'),
     path('editar/<int:pk>/', views.CargoEditView.as_view(), name='cargo_editar'),
     path('excluir/<int:pk>/', views.CargoDeleteView.as_view(), name='cargo_excluir'),
+    # Vinculos URL's
+    path('vinculos/', views.VinculosListView.as_view(), name='vinculos_lista'),
+    path('vinculos/novo/', views.VinculosCreate.as_view(), name='vinculos_novo'),
 ]

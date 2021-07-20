b9a9847bdf534862fa63e28a1378e7c313604567
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-19 22:13:42 -0300

    Implemented Vinculo form

---
+++ apps/cargos/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Vinculo
+
+
+class VinculoForm(forms.ModelForm):
+
+    class Meta:
+        model = Vinculo
+        fields = ['funcionario', 'cargo', 'matricula', 'data_inicio', 'data_fim']
+        labels = {
+            'funcionario': 'Funcionário',
+            'cargo': 'Cargo',
+            'matricula': 'Matrícula',
+            'data_inicio': 'Data de Admissão',
+            'data_fim': 'Data de Demissão',
+        }

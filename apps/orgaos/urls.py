b024208cbb6c3fdc8c059edaaaffae1ed4f1fbe5
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-23 23:03:34 -0300

    Refactored view to use class based view to list orgaos

--- apps/orgaos/urls.py
+++ apps/orgaos/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     # Orgaos URL's
-    path('', views.OrgaosList, name='orgaos_lista'),
+    path('', views.OrgaoListView.as_view(), name='orgaos_lista'),
 
 ]
--- apps/orgaos/views.py
+++ apps/orgaos/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 from .models import Orgao
 
+class OrgaoListView(TemplateView):
+    template_name = "orgaos/orgaos.html"
 
-def OrgaosList(request):
-    return render(request, "orgaos/orgaos.html", {
-        'orgaos': Orgao.objects.all()
-    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['orgaos'] = Orgao.objects.all()
+        return context

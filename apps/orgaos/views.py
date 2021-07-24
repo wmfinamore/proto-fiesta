9b849ae4b6ef4f1faf7eeaa22eaadb7b38b1557b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-24 13:01:02 -0300

    Implemented create orgao view

--- apps/orgaos/views.py
+++ apps/orgaos/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import (
+                                    TemplateView,
+                                    CreateView
+                                 )
 from .models import Orgao
 
-class OrgaoListView(TemplateView):
+
+class OrgaosListView(TemplateView):
     template_name = "orgaos/orgaos.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['orgaos'] = Orgao.objects.all()
         return context
+
+
+class OrgaoCreateView(CreateView):
+    model = Orgao
+    fields = ['nome', 'sigla', 'parent', 'tipo', 'situacao', ]
+    success_url = '/orgaos/'
+    success_message = "Órgão foi criado com sucesso!"

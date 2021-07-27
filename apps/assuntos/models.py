8b2d7b91a836e8ee232cd595fdb3d1c4492af7b3
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-26 21:49:26 -0300

    Implemented assunto model

--- apps/assuntos/models.py
+++ apps/assuntos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+# Situação da unidade administrativa
+SITUACAO_UNIDADE = [
+    ('A', 'Ativa'),
+    ('I', 'Inativa')
+]
+
+
+class Assunto(MPTTModel):
+    nome = models.CharField(max_length=50)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.nome

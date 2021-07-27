212baf600ed73fa9f6abe3ccd5e193c78ff2c432
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-26 21:51:30 -0300

    Included situacao field in assunto model

--- apps/assuntos/models.py
+++ apps/assuntos/models.py
@@ -12,6 +12,7 @@ SITUACAO_UNIDADE = [
 class Assunto(MPTTModel):
     nome = models.CharField(max_length=50)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    situacao = models.CharField(max_length=2, choices=SITUACAO_UNIDADE, default='A')
 
     def __str__(self):
         return self.nome

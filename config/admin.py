62621fab43cdf9e834adaaa0dc318d8cacf66c45
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-26 22:41:48 -0300

    Customized admin labels

--- config/admin.py
+++ config/admin.py
@@ -3,3 +3,5 @@ from django.contrib.admin import AdminSite
 
 class ProtoAdminSite(AdminSite):
     site_header = 'Administração do Protocolo'
+    site_title = 'Administração do Protocolo'
+    index_title = 'Administração do Sistema'

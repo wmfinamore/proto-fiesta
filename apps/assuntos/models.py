22efddf366fb3fd4e138d0c8a76719875e52f97e
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-28 23:37:25 -0300

    Implemented function get absolute url

--- apps/assuntos/models.py
+++ apps/assuntos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from django.urls import reverse
 
 
 # Situação do Assunto
@@ -13,6 +14,9 @@ class Assunto(MPTTModel):
     nome = models.CharField(max_length=50)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
     situacao = models.CharField(max_length=2, choices=SITUACAO_UNIDADE, default='A')
+
+    def get_absolut_url(self):
+        return reverse('assuntos_lista')
 
     def __str__(self):
         return self.nome

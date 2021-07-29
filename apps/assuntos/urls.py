8a9b04bc410236b66d1b6fdd17fb39a1359ab84f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-28 23:36:19 -0300

    Fixed bug

--- apps/assuntos/urls.py
+++ apps/assuntos/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # Assuntos URL's
     path('', views.AssuntosListView.as_view(), name='assuntos_lista'),
     path('novo/', views.AssuntoCreateView.as_view(), name='assunto_novo'),
-    path('editar/<int:pk>', views.AssuntoEditView.as_view(), name='assunto_editar'),
+    path('editar/<int:pk>/', views.AssuntoEditView.as_view(), name='assunto_editar'),
 ]

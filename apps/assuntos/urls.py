a38f54279026b073b4f76c5628e031948745d033
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-28 23:06:34 -0300

    Implemented route to assunto create view

--- apps/assuntos/urls.py
+++ apps/assuntos/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # Assuntos URL's
     path('', views.AssuntosListView.as_view(), name='assuntos_lista'),
+    path('novo/', views.AssuntoCreateView.as_view(), name='assunto_novo'),
 ]

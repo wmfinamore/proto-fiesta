2eb516b38e1600c57e83f2a5bed9207faf2b4fe3
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-28 23:06:52 -0300

    Implemented assunto create view

--- apps/assuntos/views.py
+++ apps/assuntos/views.py
@@ -1,7 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import (
+                                    ListView,
+                                    CreateView,
+                                 )
 from .models import Assunto
 
 
 class AssuntosListView(ListView):
     model = Assunto
     context_object_name = 'assuntos'
+
+
+class AssuntoCreateView(CreateView):
+    model = Assunto
+    fields = ['nome', 'parent', 'situacao']
+    success_url = '/assuntos/'
+    success_message = 'Assuntos criado com sucesso!'

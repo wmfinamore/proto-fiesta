e4d2e4e0a8b236bd206c7e2dea8c52ae4f71b34c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-31 22:39:28 -0300

    Implemented tests in core app to test routes for home

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexTests(SimpleTestCase):
+
+    # Teste de resposta a rota da homepage
+    def test_index_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    # Teste de resposta ao nome da rota para a homepage
+    def test_index_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)

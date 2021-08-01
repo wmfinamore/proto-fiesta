a2bb5fda1284e2e75a9d58ca43030c09a8a8ef67
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-07-31 22:57:28 -0300

    Refactored to exclude code repeated. Included test about html returned and view used to resolve route to homepage

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,20 +1,32 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from .views import Home
 
 
 class IndexTests(SimpleTestCase):
 
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     # Teste de resposta a rota da homepage
     def test_index_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    # Teste de resposta ao nome da rota para a homepage
-    def test_index_url_name(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     # Teste para verificar o template retornado pela rota da homepage
     def test_index_template(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'core/index.html')
+        self.assertTemplateUsed(self.response, 'core/index.html')
+
+    # Teste sobre o código html do template retornado
+    def test_index_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be in the page.'
+        )
+
+    # Teste que verifica a view usada para resolver a rota para a homepage
+    def test_index_url_resolves_home(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            Home.as_view().__name__
+        )

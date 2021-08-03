233da43dfda2bc26268d2fc2061d7406fd9c43b8
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-02 22:26:24 -0300

    Implemented test in assuntos app to verify the create assunto and the route for list assuntos

--- apps/assuntos/tests.py
+++ apps/assuntos/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Assunto
 
-# Create your tests here.
+
+class AssuntoTests(TestCase):
+
+    def setUp(self):
+        self.assunto = Assunto.objects.create(
+            nome = 'Teste de assunto',
+            situacao = 'A'
+        )
+
+    def test_assunto_listing(self):
+        self.assertEqual(f'{self.assunto.nome}', 'Teste de assunto')
+        self.assertEqual(f'{self.assunto.situacao}', 'A')
+
+    def test_assunto_list_view(self):
+        response = self.client.get(reverse('assuntos_lista'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Teste de')
+        self.assertTemplateUsed(response, 'assuntos/assunto_list.html')

8db8456b7b1b163755f71d21d8c56ea2df80a235
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-03 22:47:54 -0300

    Implemented Processos model

--- apps/processos/models.py
+++ apps/processos/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from apps.assuntos.models import Assunto
+from django.contrib.auth import get_user_model
+import uuid
 
-# Create your models here.
+# Situação do Processo
+SITUACAO_UNIDADE = [
+    ('A', 'Ativo'),
+    ('E', 'Encerrado')
+]
+
+Usuario = get_user_model()
+
+
+class Processo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    num_processo = models.PositiveBigIntegerField(unique_for_year=True, auto_created=True, editable=False)
+    interessado = models.CharField(max_length=200)
+    assunto = models.ForeignKey(Assunto, on_delete=models.PROTECT, related_name='processos_assunto')
+    resumo = models.TextField(null=True, blank=True)
+    situacao = models.CharField(max_length=2, choices=SITUACAO_UNIDADE, default='A')
+    data_criacao = models.DateTimeField(auto_now_add=True, editable=False)
+    data_atualizacao = models.DateTimeField(auto_now=True, editable=False)
+    usuario_criacao = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='processos_usuario')
+    usuario_alteracao = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='processos_alterados')
+
+    def __str__(self):
+        ano = self.data_criacao.strftime("%Y")
+        return str(self.num_processo) + '/' + str(ano)

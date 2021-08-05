109014e854cd63e272524adb3ae367b995225dd7
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-04 22:55:38 -0300

    Implemented method to auto-increment processo number by each year

--- apps/processos/models.py
+++ apps/processos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from apps.assuntos.models import Assunto
 from django.contrib.auth import get_user_model
 import uuid
+from datetime import date
 
 # Situação do Processo
 SITUACAO_UNIDADE = [
@@ -14,7 +15,8 @@ Usuario = get_user_model()
 
 class Processo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    num_processo = models.PositiveBigIntegerField(unique_for_year=True, editable=False)
+    num_processo = models.PositiveBigIntegerField(
+        unique_for_year=date.today().strftime("%Y"), editable=False, default=0)
     interessado = models.CharField(max_length=200)
     assunto = models.ForeignKey(Assunto, on_delete=models.PROTECT, related_name='processos_assunto')
     resumo = models.TextField(null=True, blank=True)
@@ -26,6 +28,24 @@ class Processo(models.Model):
         Usuario, on_delete=models.PROTECT, related_name='processos_alterados', null=True, blank=True)
 
     def save(self, *args, **kwargs):
+        # verifica se estamos inserindo um novo processo(=0) ou se é uma atualização(>0)
+        if self.num_processo > 0:
+            # Se for uma atualização apenas atualiza o objeto .save
+            pass
+        # Caso seja um novo processo, executa o procedimento para auto-incremento
+        else:
+            # Tenta recuperar o último processo criado no ano corrente
+            processo = Processo.objects.filter(data_criacao__year=date.today().strftime("%Y"))\
+                .order_by('-num_processo')\
+                .first()
+            # caso tenha um processo criado no ano, usa o número do processo para incrementar a numeração do novo
+            # processo
+            if processo:
+                self.num_processo = int(processo.num_processo) + 1
+            else:
+                # Caso seja o primeiro processo do ano atribui o número 1
+                self.num_processo = 1
+                # salvamos o objeto com o método .save da superclasse
         super().save(*args, **kwargs)
 
     def __str__(self):

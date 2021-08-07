3a189ceddcebd1899d3b5013081b661e3fcd968b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-07 16:32:58 -0300

    Implemented route to processo update view

--- apps/processos/urls.py
+++ apps/processos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProcessosListView, ProcessoCreateView
+from .views import ProcessosListView, ProcessoCreateView, ProcessoUpdateView
 
 urlpatterns = [
     path('', ProcessosListView.as_view(), name='processos_lista'),
     path('novo/', ProcessoCreateView.as_view(), name='processo_novo'),
+    path('editar/<int:pk>', ProcessoUpdateView.as_view(), name='processo_editar'),
 ]

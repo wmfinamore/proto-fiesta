95265b1090efe83909774d0c2c49a7e156c87be6
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-07 17:18:07 -0300

    Implemented route to processo delete view

--- apps/processos/urls.py
+++ apps/processos/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import ProcessosListView, ProcessoCreateView, ProcessoUpdateView
+from .views import (ProcessosListView,
+                    ProcessoCreateView,
+                    ProcessoUpdateView,
+                    ProcessoDeleteView)
 
 urlpatterns = [
     path('', ProcessosListView.as_view(), name='processos_lista'),
     path('novo/', ProcessoCreateView.as_view(), name='processo_novo'),
     path('editar/<pk>', ProcessoUpdateView.as_view(), name='processo_editar'),
+    path('excluir/<pk>', ProcessoDeleteView.as_view(), name='processo_excluir'),
 ]

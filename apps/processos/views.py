acf60cdcae5127b18c545967d663de93d9f5755e
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-07 16:39:11 -0300

    Implemented processo update view

--- apps/processos/views.py
+++ apps/processos/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Processo
 
 class ProcessosListView(ListView):
@@ -22,3 +22,19 @@ class ProcessoCreateView(CreateView):
         processo.save()
         # retornar o fommulário válido para a superclasse
         return super(ProcessoCreateView, self).form_valid(form)
+
+
+class ProcessoUpdateView(UpdateView):
+    model = Processo
+    fields = ['interessado', 'assunto', 'resumo', 'situacao']
+    context_object_name = 'processo'
+
+    def form_valid(self, form):
+        # recupera o formulário enviado no post, antes do commit
+        processo = form.save(commit=False)
+        # insere o usuario informado no request
+        processo.usuario_alteracao = self.request.user
+        # Salva o model processo
+        processo.save()
+        # retornar o fommulário válido para a superclasse
+        return super(ProcessoUpdateView, self).form_valid(form)

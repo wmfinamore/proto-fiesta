3e87ea0286803bd9a330f9b3117f26c97ca6b6d6
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-06 22:58:28 -0300

    Implemented route to processo create view

--- apps/processos/urls.py
+++ apps/processos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProcessosListView
+from .views import ProcessosListView, ProcessoCreateView
 
 urlpatterns = [
     path('', ProcessosListView.as_view(), name='processos_lista'),
+    path('novo/', ProcessoCreateView.as_view(), name='processo_novo'),
 ]

3fdf08d1b74e9355526584a9a008efd5f453db9b
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-11 22:33:18 -0300

    Implemented and migrate the tramite model

--- apps/tramitacoes/models.py
+++ apps/tramitacoes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from apps.processos.models import Processo
+from django.contrib.auth import get_user_model
+from apps.orgaos.models import Orgao
+import uuid
 
-# Create your models here.
+Usuario = get_user_model()
+
+
+class Tramite(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    processo = models.ForeignKey(Processo, on_delete=models.PROTECT, related_name='processo_tramites')
+    orgao_destino = models.ForeignKey(Orgao, on_delete=models.PROTECT, related_name='orgao_tramites')
+    despacho = models.TextField(null=True, blank=True)
+    data_tramite = models.DateTimeField(auto_now_add=True)
+    usuario_tramite = models.ForeignKey(Usuario, on_delete=models.PROTECT, related_name='usuario_tramites')
+    data_recebimento = models.DateTimeField(null=True, blank=True)
+    usuario_recepcao = models.ForeignKey(Usuario, on_delete=models.PROTECT, null=True, blank=True, related_name='usuario_recepcoes')
+
+    def __str__(self):
+        return str(self.processo.numero_processo) + '-' + str(self.orgao_destino.nome)
+
+    class Meta:
+        ordering = ['-data_tramite']

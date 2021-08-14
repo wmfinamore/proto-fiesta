d8159699b4c7794697e7269201cc5b44c27eba36
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-14 16:03:09 -0300

    Implemented route to tramitacao update view and tramitacao delete view

--- apps/tramitacoes/urls.py
+++ apps/tramitacoes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TramitacaoCreateView
+from .views import TramitacaoCreateView, TramitacaoUpdateView, TramitacaoDeleteView
 
 
 urlpatterns = [
-    path('novo/<pk>/', TramitacaoCreateView.as_view(), name='tramitacao_novo'),
+    path('novo/<pk>', TramitacaoCreateView.as_view(), name='tramitacao_novo'),
+    path('receber/<pk>', TramitacaoUpdateView.as_view(), name='tramitacao_receber'),
+    path('excluir/<pk>', TramitacaoDeleteView.as_view(), name='tramitacao_excluir'),
 ]

ce22dd60ff4fbac28918d1a1d6b573abdd7cddcd
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-14 17:05:57 -0300

    Removed field no editable. Included depacho field. Included data_recebimento field with widget

--- apps/tramitacoes/forms.py
+++ apps/tramitacoes/forms.py
@@ -3,12 +3,14 @@ from .models import Tramite
 
 
 class TramiteForm(forms.ModelForm):
-    def __init__(self, user, *args, **kwargs):
+    data_recebimento = forms.DateField(widget=forms.widgets.NumberInput(attrs={'type': 'date'}))
+
+    def __init__(self, *args, **kwargs):
         super(TramiteForm, self).__init__(*args, **kwargs)
         # definidos os campos que serão apenas de leitura na criação do formulário
         self.fields['processo'].disabled = True
+        self.fields['despacho'].disabled = True
         self.fields['orgao_destino'].disabled = True
-        self.fields['data_tramite'].disabled = True
         self.fields['usuario_tramite'].disabled = True
 
     class Meta:
@@ -16,7 +18,6 @@ class TramiteForm(forms.ModelForm):
         fields = ['processo',
                   'orgao_destino',
                   'despacho',
-                  'data_tramite',
                   'usuario_tramite',
                   'data_recebimento',
                   ]
@@ -24,7 +25,6 @@ class TramiteForm(forms.ModelForm):
             'processo': 'Processo',
             'orgao_destino': 'Órgão Destino',
             'despacho': 'Despacho',
-            'data_tramite': 'Data Trâmite',
             'usuario_tramite': 'Usuário Trâmite',
             'data_recebimento': 'Data Recepção',
         }

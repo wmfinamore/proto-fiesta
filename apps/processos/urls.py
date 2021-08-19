555bf34050f41d69b6680f212be4570b6e2e007c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-18 23:28:13 -0300

    Implemented route to Caixa List View

--- apps/processos/urls.py
+++ apps/processos/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (ProcessosListView,
                     ProcessoCreateView,
                     ProcessoUpdateView,
-                    ProcessoDeleteView)
+                    ProcessoDeleteView,
+                    CaixaListVIew)
 
 urlpatterns = [
     path('', ProcessosListView.as_view(), name='processos_lista'),
     path('novo/', ProcessoCreateView.as_view(), name='processo_novo'),
     path('editar/<pk>', ProcessoUpdateView.as_view(), name='processo_editar'),
     path('excluir/<pk>', ProcessoDeleteView.as_view(), name='processo_excluir'),
+    path('caixa/', CaixaListVIew.as_view(), name='caixa_lista'),
 ]

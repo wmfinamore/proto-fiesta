7dee17d2e5d4008f62df4d0c9c59dbaa1347d671
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-30 23:20:21 -0300

    Implemented nested relacionship for assuntos app in processo api

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 from apps.processos.models import Processo
+from apps.assuntos.models import Assunto
+
+
+# Classe implementada para incluir Nested Relationships
+class AssuntoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assunto
+        fields = ['id', 'nome', 'situacao', 'level']
 
 
 # Classe que converte os dados para JSON
 class ProcessoSerializer(serializers.ModelSerializer):
+    assunto = AssuntoSerializer(many=False, read_only=True)
+
     class Meta:
         model = Processo
         fields = ('id',

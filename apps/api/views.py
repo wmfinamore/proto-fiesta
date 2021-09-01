3467a53c530302baa569261c840f95c778ef89dd
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-31 23:27:36 -0300

    IMplemented viewset for Orgao, Assunto and User serializers

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
 from apps.processos.models import Processo
+from apps.orgaos.models import Orgao
 from apps.assuntos.models import Assunto
-from .serializers import ProcessoSerializer, AssuntoSerializer, Orgao, UserSerializer
+from django.contrib.auth import get_user_model
+from .serializers import ProcessoSerializer, AssuntoSerializer, OrgaoSerializer, UserSerializer
+
+User = get_user_model()
 
 
 class ProcessoAPIView(viewsets.ModelViewSet):
@@ -12,9 +16,25 @@ class ProcessoAPIView(viewsets.ModelViewSet):
     serializer_class = ProcessoSerializer
 
 
+class OrgaoAPIView(viewsets.ModelViewSet):
+    """
+    Lista todos os órgãos cadastrados no sistema
+    """
+    queryset = Orgao.objects.all()
+    serializer_class = OrgaoSerializer
+
+
 class AssuntoAPIView(viewsets.ModelViewSet):
     """
     Lista todos os assuntos cadastrados no sistema
     """
     queryset = Assunto.objects.all()
     serializer_class = AssuntoSerializer
+
+
+class UserAPIView(viewsets.ModelViewSet):
+    """
+    Lista todos os usuários cadastrados no sistema
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

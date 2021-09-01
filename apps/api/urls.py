d0a03a320745fed21616db4de19c36beb301eb7f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-31 23:29:27 -0300

    Change the router to use DefaultRouter. Included endpoint to processos, assuntos, orgaos and usuarios viewset

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
-from rest_framework.routers import SimpleRouter
-from .views import ProcessoAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProcessoAPIView, OrgaoAPIView, AssuntoAPIView, UserAPIView
 
 
-router = SimpleRouter()
-router.register('', ProcessoAPIView, basename='processos')
+router = DefaultRouter()
+router.register('processos', ProcessoAPIView)
+router.register('assuntos', AssuntoAPIView)
+router.register('orgaos', OrgaoAPIView)
+router.register('usuarios', UserAPIView)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+]

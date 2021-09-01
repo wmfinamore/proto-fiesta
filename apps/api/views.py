27aba1282a50ab31a8a704bc5a4cdad9971bf7b1
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-08-31 22:43:53 -0300

    Implemented viewset to aggregate api's

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,11 +1,14 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from apps.processos.models import Processo
-from .serializers import ProcessoSerializer
+from .serializers import ProcessoSerializer, AssuntoSerializer, Orgao, UserSerializer
 
 
-class ProcessoAPIView(generics.ListAPIView):
+class ProcessoAPIView(viewsets.ModelViewSet):
     """
     Lista todos os processos cadastrados no sistema
     """
     queryset = Processo.objects.all()
     serializer_class = ProcessoSerializer
+
+
+class Assunto

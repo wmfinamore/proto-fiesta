287e56b842835760bba335e3d4beb3f29de1fc16
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-09-06 22:41:47 -0300

    Implemented rout to search function

--- apps/processos/urls.py
+++ apps/processos/urls.py
@@ -3,7 +3,8 @@ from .views import (ProcessosListView,
                     ProcessoCreateView,
                     ProcessoUpdateView,
                     ProcessoDeleteView,
-                    CaixaListView,)
+                    CaixaListView,
+                    processo_search)
 
 urlpatterns = [
     path('', ProcessosListView.as_view(), name='processos_lista'),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('editar/<pk>', ProcessoUpdateView.as_view(), name='processo_editar'),
     path('excluir/<pk>', ProcessoDeleteView.as_view(), name='processo_excluir'),
     path('caixa/', CaixaListView.as_view(), name='caixa_lista'),
+    path('pesquisar/', processo_search, name='processo_search'),
 ]

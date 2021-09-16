305a9af8455964ba45d9660b32edf3d0dbba2b60
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-09-15 22:14:34 -0300

    Configured static and media files for production

--- config/settings.py
+++ config/settings.py
@@ -139,11 +139,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# MEDIA FILES
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

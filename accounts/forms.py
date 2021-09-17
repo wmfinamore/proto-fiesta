4d66873df4a26d5bb4103f718a81307084309e63
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-09-16 23:23:30 -0300

    Included javascript in admin module to test field mask

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 # get_user_model retorna o modelo indicado em AUTH_USER_MODEL no arquivo settings.py
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -8,6 +9,11 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username', 'cpf')
+        widgets = {'cpf': forms.TextInput(attrs={'class': "cpf"})}
+
+    class Media:
+        js = ("admin/js/jquery.mask.js",
+              "admin/js/mask.js",)
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -15,3 +21,8 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('email', 'username', 'cpf')
+        widgets = {'cpf': forms.TextInput(attrs={'class': "cpf"})}
+
+    class Media:
+        js = ("admin/js/jquery.mask.js",
+              "admin/js/mask.js",)

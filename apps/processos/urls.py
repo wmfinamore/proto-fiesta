9ba10e6ed8f1f415efb1afa44011323580282818
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-09-17 22:51:03 -0300

    Included route to peocesso detail view

--- apps/processos/urls.py
+++ apps/processos/urls.py
@@ -3,12 +3,14 @@ from .views import (ProcessosListView,
                     ProcessoCreateView,
                     ProcessoUpdateView,
                     ProcessoDeleteView,
+                    ProcessoDetailView,
                     CaixaListView,
                     processo_search)
 
 urlpatterns = [
     path('', ProcessosListView.as_view(), name='processos_lista'),
     path('novo/', ProcessoCreateView.as_view(), name='processo_novo'),
+    path('detalhe/<pk>', ProcessoDetailView.as_view(), name='processo_detalhar'),
     path('editar/<pk>', ProcessoUpdateView.as_view(), name='processo_editar'),
     path('excluir/<pk>', ProcessoDeleteView.as_view(), name='processo_excluir'),
     path('caixa/', CaixaListView.as_view(), name='caixa_lista'),

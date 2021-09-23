bb9bfa1839aea56f4c295592adbeb75b936f172d
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-09-22 22:16:14 -0300

    Implemented route to Interessado Create View

--- apps/interessados/urls.py
+++ apps/interessados/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import interessadosapi, InteressadosListView
+from .views import interessadosapi, InteressadosListView, InteressadoCreateView
 
 
 urlpatterns = [
     path('', InteressadosListView.as_view(), name='interessados_lista'),
+    path('novo/', InteressadoCreateView.as_view(), name='interessado_novo'),
     path('api/', interessadosapi, name='InteressadosApi'),
 ]

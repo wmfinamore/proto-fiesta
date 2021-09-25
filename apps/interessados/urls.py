7a00044c4c76c200e1047fe37e315a830a8b15b1
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-09-24 22:12:52 -0300

    Implemented route to Interessado update view

--- apps/interessados/urls.py
+++ apps/interessados/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import interessadosapi, InteressadosListView, InteressadoCreateView
+from .views import interessadosapi, InteressadosListView, InteressadoCreateView, InteressadoUpdateView
 
 
 urlpatterns = [
     path('', InteressadosListView.as_view(), name='interessados_lista'),
     path('novo/', InteressadoCreateView.as_view(), name='interessado_novo'),
+    path('editar/<int:pk>/', InteressadoUpdateView.as_view(), name='interessado_editar'),
     path('api/', interessadosapi, name='InteressadosApi'),
 ]

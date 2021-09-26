97d8403e7ca6262d23a9ef0272f0dcf0f33a9ed3
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-09-25 23:19:58 -0300

    Implemented route to Interessado Delete View

--- apps/interessados/urls.py
+++ apps/interessados/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import interessadosapi, InteressadosListView, InteressadoCreateView, InteressadoUpdateView
+from .views import (interessadosapi,
+                    InteressadosListView,
+                    InteressadoCreateView,
+                    InteressadoUpdateView,
+                    InteressadoDeleteView,)
 
 
 urlpatterns = [
     path('', InteressadosListView.as_view(), name='interessados_lista'),
     path('novo/', InteressadoCreateView.as_view(), name='interessado_novo'),
     path('editar/<pk>/', InteressadoUpdateView.as_view(), name='interessado_editar'),
+    path('excluir/<pk>/', InteressadoDeleteView.as_view(), name='interessado_excluir'),
     path('api/', interessadosapi, name='InteressadosApi'),
 ]

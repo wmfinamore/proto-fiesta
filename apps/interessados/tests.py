bf3eb7cf497aac102b38a64750ea06735ea772f5
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-09-29 22:29:38 -0300

    Implemented unit test for interessados app

--- apps/interessados/tests.py
+++ apps/interessados/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Interessado
 
-# Create your tests here.
+
+class InteressadoTest(TestCase):
+    def setUp(self):
+        self.interessado = Interessado.objects.create(
+        nome = 'Fulano da Silva',
+        nome_social = 'Fulaninho',
+        cpf = '036.784.000-60',
+        cnpj = '78265456000170',
+        )
+
+    # Teste de recuperação de instância da classe Interessado
+    def test_interessado_listing(self):
+        self.assertEqual(f'{self.interessado.nome}', 'Fulano da Silva')
+        self.assertEqual(f'{self.interessado.nome_social}', 'Fulaninho')
+        self.assertEqual(f'{self.interessado.cpf}', '036.784.000-60')
+        self.assertEqual(f'{self.interessado.cnpj}', '78265456000170')

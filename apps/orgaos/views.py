80f5206f9818abb91c26502e54ef8a2c06ede3c3
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-11-01 15:53:02 -0300

    Refactored orgaos view to include Permission Required for the endpoints

--- apps/orgaos/views.py
+++ apps/orgaos/views.py
@@ -6,7 +6,7 @@ from django.views.generic import (
                                  )
 from .models import Orgao
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
 class OrgaosListView(TemplateView):
@@ -18,20 +18,32 @@ class OrgaosListView(TemplateView):
         return context
 
 
-class OrgaoCreateView(LoginRequiredMixin, CreateView):
+class OrgaoCreateView(LoginRequiredMixin,
+                      PermissionRequiredMixin,
+                      CreateView,):
+    permission_required = ('orgaos.add_orgao',)
+    permission_denied_message = "Você não tem permissão para visualizar adicionar órgãos"
     model = Orgao
     fields = ['nome', 'sigla', 'parent', 'tipo', 'situacao', ]
     success_url = '/orgaos/'
     success_message = "Órgão foi criado com sucesso!"
 
 
-class OrgaoEditView(LoginRequiredMixin, UpdateView):
+class OrgaoEditView(LoginRequiredMixin,
+                    PermissionRequiredMixin,
+                    UpdateView):
+    permission_required = ('orgaos.change_orgao',)
+    permission_denied_message = "Você não tem permissão para editar órgãos"
     model = Orgao
     fields = ['nome', 'sigla', 'parent', 'tipo', 'situacao', ]
     context_object_name = 'orgao'
 
 
-class OrgaoDeleteView(LoginRequiredMixin, DeleteView):
+class OrgaoDeleteView(LoginRequiredMixin,
+                      PermissionRequiredMixin,
+                      DeleteView):
+    permission_required = ('orgaos.delete_orgao',)
+    permission_denied_message = "Você não tem permissão para excluir órgãos"
     model = Orgao
     success_url = reverse_lazy('orgaos_lista')
     context_object_name = 'orgao'

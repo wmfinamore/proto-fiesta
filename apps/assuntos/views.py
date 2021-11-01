45655ce5480c51d1fad2eca45c8a187ae2dfee95
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-11-01 16:09:55 -0300

    Refactored assuntos view to include Permission Required for the endpoints

--- apps/assuntos/views.py
+++ apps/assuntos/views.py
@@ -6,7 +6,7 @@ from django.views.generic import (
                                  )
 from .models import Assunto
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
 class AssuntosListView(ListView):
@@ -14,20 +14,32 @@ class AssuntosListView(ListView):
     context_object_name = 'assuntos'
 
 
-class AssuntoCreateView(LoginRequiredMixin, CreateView):
+class AssuntoCreateView(LoginRequiredMixin,
+                        PermissionRequiredMixin,
+                        CreateView):
+    permission_required = ('assuntos.add_assunto')
+    permission_denied_message = "Você não tem permissão para adicionar assuntos"
     model = Assunto
     fields = ['nome', 'parent', 'situacao']
     success_url = '/assuntos/'
     success_message = 'Assuntos criado com sucesso!'
 
 
-class AssuntoEditView(LoginRequiredMixin, UpdateView):
+class AssuntoEditView(LoginRequiredMixin,
+                      PermissionRequiredMixin,
+                      UpdateView):
+    permission_required = ('assuntos.change_assunto')
+    permission_denied_message = "Você não tem permissão para alterar assuntos"
     model = Assunto
     fields = ['nome', 'parent', 'situacao']
     context_object_name = 'assunto'
 
 
-class AssuntoDeleteView(LoginRequiredMixin, DeleteView):
+class AssuntoDeleteView(LoginRequiredMixin,
+                        PermissionRequiredMixin,
+                        DeleteView):
+    permission_required = ('assuntos.delete_assunto')
+    permission_denied_message = "Você não tem permissão para excluir assuntos"
     model = Assunto
     context_object_name = 'assunto'
     success_url = reverse_lazy('assuntos_lista')

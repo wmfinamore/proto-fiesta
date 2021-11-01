c69e859aac8de19875abdac270a7f9821170c6aa
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-11-01 14:59:02 -0300

    Configured environment for development again

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", config("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env.bool("DJANGO_DEBUG", default=False)
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS", config("DJANGO_ALLOWED_HOSTS")).split(" ")
 

a89e061a6b8cda62bff967faec6f64b7f306941c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-11-01 16:03:50 -0300

    Refactored cargo view and vinculo view to include Permission Required for the endpoints

--- apps/cargos/views.py
+++ apps/cargos/views.py
@@ -9,7 +9,7 @@ from django.views.generic import (
 from .models import Cargo
 from .models import Vinculo
 from .forms import VinculoForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
 class CargosListView(ListView):
@@ -17,20 +17,33 @@ class CargosListView(ListView):
     context_object_name = 'cargos'
 
 
-class CargoCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+class CargoCreateView(LoginRequiredMixin,
+                      PermissionRequiredMixin,
+                      SuccessMessageMixin,
+                      CreateView):
+    permission_required = ('cargos.add_cargo')
+    permission_denied_message = "Você não tem permissão para adicionar Cargos"
     model = Cargo
     fields = ['classe', 'nome', 'jornada',]
     success_url = '/cargos/'
     success_message = "Cargo foi criado com sucesso!"
 
 
-class CargoEditView(LoginRequiredMixin, UpdateView):
+class CargoEditView(LoginRequiredMixin,
+                    PermissionRequiredMixin,
+                    UpdateView):
+    permission_required = ('cargos.change_cargo')
+    permission_denied_message = "Você não tem permissão para alterar cargos"
     model = Cargo
     fields = ['classe', 'nome', 'jornada',]
     context_object_name = 'cargo'
 
 
-class CargoDeleteView(LoginRequiredMixin, DeleteView):
+class CargoDeleteView(LoginRequiredMixin,
+                      PermissionRequiredMixin,
+                      DeleteView):
+    permission_required = ('cargos.delete_cargo')
+    permission_denied_message = "Você não tem permissão para excluir cargos"
     model = Cargo
     success_url = reverse_lazy('cargos_lista')
     context_object_name = 'cargo'
@@ -41,20 +54,32 @@ class VinculosListView(ListView):
     context_object_name = 'vinculos'
 
 
-class VinculoCreateView(LoginRequiredMixin, CreateView):
+class VinculoCreateView(LoginRequiredMixin,
+                        PermissionRequiredMixin,
+                        CreateView):
+    permission_required = ('cargos.add_vinculo')
+    permission_denied_message = "Você não tem permissão para adicionar vínculos"
     model = Vinculo
     form_class = VinculoForm
     success_url = '/cargos/vinculos/'
     success_message = "Vínculo foi criado com sucesso!"
 
 
-class VinculoEditView(LoginRequiredMixin, UpdateView):
+class VinculoEditView(LoginRequiredMixin,
+                      PermissionRequiredMixin,
+                      UpdateView):
+    permission_required = ('cargos.change_vinculo')
+    permission_denied_message = "Você não tem permissão para alterar vínculos"
     model = Vinculo
     form_class = VinculoForm
     context_object_name = 'vinculo'
 
 
-class VinculoDeleteView(LoginRequiredMixin, DeleteView):
+class VinculoDeleteView(LoginRequiredMixin,
+                        PermissionRequiredMixin,
+                        DeleteView):
+    permission_required = ('cargos.delete_vinculo')
+    permission_denied_message = "Você não tem permissão para excluir vínculos"
     model = Vinculo
     success_url = reverse_lazy('vinculos_lista')
     context_object_name = 'vinculo'

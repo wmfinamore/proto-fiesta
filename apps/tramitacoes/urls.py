72e0fe70c2ddf643e4070759b943b719ac4cbf53
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-11-15 20:53:39 -0300

    Implemented route to view Receber Processo

--- apps/tramitacoes/urls.py
+++ apps/tramitacoes/urls.py
@@ -3,12 +3,14 @@ from .views import (TramitacaoCreateView,
                     TramitacaoUpdateView,
                     TramitacaoDeleteView,
                     ExportarTramitesCSV,
-                    TramitesNaoRecebidos,)
+                    TramitesNaoRecebidos,
+                    ReceberProcesso,)
 
 
 urlpatterns = [
     path('novo/<pk>', TramitacaoCreateView.as_view(), name='tramitacao_novo'),
     path('receber/<pk>', TramitacaoUpdateView.as_view(), name='tramitacao_receber'),
+    path('receber-processo/<pk>', ReceberProcesso.as_view(), name='processo_receber'),
     path('excluir/<pk>', TramitacaoDeleteView.as_view(), name='tramitacao_excluir'),
     path('exportar-tramites-csv/<pk>', ExportarTramitesCSV.as_view(), name='exportar_tramites_csv'),
     path('tramites-nao-recebidos/', TramitesNaoRecebidos.as_view(), name='tramites_nao_recebidos'),

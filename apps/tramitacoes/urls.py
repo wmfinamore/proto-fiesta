3822f51e123a241102e71a84c26927a460c17f8a
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-12-04 22:53:56 -0300

    Implemented route to tramite detail

--- apps/tramitacoes/urls.py
+++ apps/tramitacoes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (TramitacaoCreateView,
                     TramitacaoUpdateView,
                     TramitacaoDeleteView,
+                    TramitacaoDetailView,
                     ExportarTramitesCSV,
                     TramitesNaoRecebidos,
                     ReceberProcesso,)
@@ -12,6 +13,8 @@ urlpatterns = [
     path('receber/<pk>', TramitacaoUpdateView.as_view(), name='tramitacao_receber'),
     path('receber-processo/<pk>', ReceberProcesso.as_view(), name='processo_receber'),
     path('excluir/<pk>', TramitacaoDeleteView.as_view(), name='tramitacao_excluir'),
+    path('detalhar/<pk>', TramitacaoDetailView.as_view(), name='tramitacao_detalhar'),
+    # Relatórios
     path('exportar-tramites-csv/<pk>', ExportarTramitesCSV.as_view(), name='exportar_tramites_csv'),
     path('tramites-nao-recebidos/', TramitesNaoRecebidos.as_view(), name='tramites_nao_recebidos'),
 ]

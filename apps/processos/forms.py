c243784f19fc8e7041c29ad4312f195fd86de0fc
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-12-12 20:11:31 -0300

    Organizing the code

--- apps/processos/forms.py
+++ apps/processos/forms.py
@@ -16,13 +16,12 @@ class ProcessoForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         # na instanciação do form não carrega os interessados no select
         self.fields['interessado'].queryset = Interessado.objects.none()
-
         if 'interessado' in self.data:
             # retorna o query com os dados para processar o POST
             self.fields['interessado'].queryset = Interessado.objects.all()
-
         elif self.instance:
             # Retorna o valor do campo interessado para Update
             self.fields['interessado'].queryset = Interessado.objects.filter(pk=self.instance.interessado.id)

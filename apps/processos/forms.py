6e911e91ba32c734e46db238f2ecdac5eee7faa1
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-12-11 22:48:25 -0300

    Implemented form class for Processos app

--- apps/processos/forms.py
+++ apps/processos/forms.py
@@ -1,5 +1,15 @@
 from django import forms
+from .models import Processo
+from apps.interessados.models import Interessado
+from apps.assuntos.models import Assunto
 
 
 class SearchForm(forms.Form):
     query = forms.CharField(label='Pesquisa')
+
+
+class ProcessoForm(forms.ModelForm):
+
+    class Meta:
+        model = Processo
+        fields = ['interessado', 'assunto', 'resumo', 'situacao']

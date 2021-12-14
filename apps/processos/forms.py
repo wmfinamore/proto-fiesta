a6a84143b77ca6301f5f9911372f1468a34710db
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-12-14 17:36:24 -0300

    Included field assunto to return right values using select2

--- apps/processos/forms.py
+++ apps/processos/forms.py
@@ -19,11 +19,14 @@ class ProcessoForm(forms.ModelForm):
 
         if not self.initial and 'interessado' not in self.data:
             self.fields['interessado'].queryset = Interessado.objects.none()
+            self.fields['assunto'].queryset = Assunto.objects.none()
         # na instanciação do form não carrega os interessados no select
         else:
             if 'interessado' in self.data:
                 # retorna o query com os dados para processar o POST
                 self.fields['interessado'].queryset = Interessado.objects.all()
+                self.fields['assunto'].queryset = Assunto.objects.all()
             elif self.instance:
                 # Retorna o valor do campo interessado para Update
                 self.fields['interessado'].queryset = Interessado.objects.filter(pk=self.instance.interessado.id)
+                self.fields['assunto'].queryset = Assunto.objects.filter(pk=self.instance.assunto.id)

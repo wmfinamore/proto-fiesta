e015bce5d3f2dd8a07e91671d19c0d1db6e91b75
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-12-24 16:57:55 -0300

    Refactored Vinculo form removing unnecessary code and changing lotacao field

--- apps/cargos/forms.py
+++ apps/cargos/forms.py
@@ -2,19 +2,10 @@ from django import forms
 from mptt.forms import TreeNodeChoiceField
 from .models import Vinculo
 from apps.orgaos.models import Orgao
-# raw_id_field admin widget
-from django.contrib.admin.widgets import ForeignKeyRawIdWidget
-from django.contrib import admin
-
-
-admin.autodiscover()
 
 
 class VinculoForm(forms.ModelForm):
-    # lotacao = TreeNodeChoiceField(queryset=Orgao.objects.all())
-    lotacao = forms.ModelChoiceField(Orgao.objects.all(),
-                                     widget=ForeignKeyRawIdWidget(Vinculo._meta.get_field("lotacao").remote_field,
-                                                                  admin.site))
+    lotacao = TreeNodeChoiceField(queryset=Orgao.objects.all())
 
     # Thanks https://www.ordinarycoders.com/blog/article/using-django-form-fields-and-widgets
     data_inicio = forms.DateField(widget=forms.widgets.NumberInput(attrs={'type': 'date'}))

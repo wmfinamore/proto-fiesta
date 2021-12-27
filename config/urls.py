a16d793ac420cd7486423b1d555a101f9d931501
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-12-26 21:34:39 -0300

    Added configuration to serve media files with debug true

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
 
 
 schema_view = get_schema_view(
@@ -56,4 +57,5 @@ if settings.DEBUG:
 
     urlpatterns = [
         path('__debug__', include(debug_toolbar.urls))
-    ] + urlpatterns
+    ] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

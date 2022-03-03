9f2f78b402465ae8e436fe4501b8ab6dfbd9159f
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-03-02 21:37:33 -0300

    Applied login_required decorator to prevent access at the cached page without user logged

--- apps/processos/urls.py
+++ apps/processos/urls.py
@@ -7,14 +7,15 @@ from .views import (ProcessosListView,
                     CaixaListView,
                     processo_search)
 from django.views.decorators.cache import cache_page
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path('', cache_page(60 * 15)(ProcessosListView.as_view()), name='processos_lista'),
+    path('', login_required(cache_page(60 * 1)(ProcessosListView.as_view())), name='processos_lista'),
     path('novo/', ProcessoCreateView.as_view(), name='processo_novo'),
     path('detalhe/<pk>', ProcessoDetailView.as_view(), name='processo_detalhar'),
     path('editar/<pk>', ProcessoUpdateView.as_view(), name='processo_editar'),
     path('excluir/<pk>', ProcessoDeleteView.as_view(), name='processo_excluir'),
-    path('caixa/', cache_page(60 * 15)(CaixaListView.as_view()), name='caixa_lista'),
+    path('caixa/', login_required(cache_page(60 * 1)(CaixaListView.as_view())), name='caixa_lista'),
     path('pesquisar/', processo_search, name='processo_search'),
 ]
